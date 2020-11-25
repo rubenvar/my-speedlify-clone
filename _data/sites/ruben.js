549192b05d739ebaeee6510a92bb9e8a1e9a2885
Rubén <hey@rubenvara.io>
2020-11-25 14:27:21 +0100

    add my sites

---
+++ _data/sites/ruben.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: 'Rubén',
+  description: 'Most of my sites',
+  options: {
+    skip: false,
+    frequency: 60 * 12, // 12 hours
+  },
+  urls: [
+    'https://quizzical-varahamihira-9fdae1.netlify.app/',
+    'https://trajesneopreno.es/',
+    'https://comousarutorrent.com/',
+    'https://sayhello.how',
+    'https://rubenvara.io',
+    'https://vpnfacil.com/',
+    'https://todohipo.com/',
+    'https://www.larascandleshop.com/',
+  ],
+};
