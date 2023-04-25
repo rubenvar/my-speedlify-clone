92d0af0f9eaaf8eff1da58705865738711dfbba0
Rubén <hey@rubenvara.io>
2023-04-25 02:03:08 +0200

    update sites

--- _data/sites/ruben.js
+++ _data/sites/ruben.js
@@ -6,11 +6,10 @@ module.exports = {
     frequency: 60 * 72, // 72 hours
   },
   urls: [
-    'https://quizzical-varahamihira-9fdae1.netlify.app/',
-    'https://trajesneopreno.es/',
-    'https://comousarutorrent.com/',
-    'https://sayhello.how',
+    'https://calendarioaguasabiertas.com/',
     'https://rubenvara.io',
+    'https://trajesneopreno.es/',
+    'https://sayhello.how',
     'https://vpnfacil.com/',
     'https://todohipo.com/',
     'https://www.larascandleshop.com/',
